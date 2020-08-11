baed27458c44dee093f083564b57a8ef3719fd23
Woth-Ursula <uwoth@kogito.network>
2020-08-11 22:14:23 +0200

    Added Ubuntu package

--- manifests/install/client.pp
+++ manifests/install/client.pp
@@ -8,6 +8,16 @@
 class sssd::install::client (
   $ensure = $::sssd::install::package_ensure
 ){
+
+  if $facts['os']['name'] in ['RedHat','CentOS','OracleLinux'] {
+    $_package = 'sssd-client'
+  }
+  elsif $facts['os']['name'] in ['Ubuntu'] {
+    $_package = 'sssd-common'
+  }
+  else {
+    fail("OS '${facts['os']['name']}' not supported by '${module_name}'")
+  }
   package { 'sssd-client':
     ensure => $ensure
   }
